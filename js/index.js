56af7cc96bb83fe16455474f41c0fded1793e9f6
Anthony Kim <anthony011@gmail.com>
2025-03-12 15:22:10 -0400

    refine

--- js/index.js
+++ js/index.js
@@ -149,7 +149,7 @@ const renderTable = () => {
   if (filteredData.length > len) {
     table.innerHTML += `
           <div id='js-load-link'>
-            <div class='more-data blue' onclick='handleLoadNext()'> &gt;&gt; ${increase}개 더 보기</div>
+            <div class='more-data blue' onclick='handleLoadNext()'> &gt;&gt; 다음 ${increase}개 보기</div>
           </div>
       `;
   }
